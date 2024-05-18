e55fd70c712c32c094bcb4015165f1b9280cb92c
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-05-19 01:39:55 +0530

    project start

---
+++ src/test/java/in/sagnikchakraborty/SmartContractManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sagnikchakraborty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContractManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
