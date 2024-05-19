a298d4774527ebfef2ae741c8903ef412c7943ba
Sagnik004 <sagnikchakraborty910@yahoo.com>
2024-05-20 00:24:50 +0530

    Added html files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./src/main/resources/**/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'roboto': ['Roboto', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
   darkMode: 'selector',
